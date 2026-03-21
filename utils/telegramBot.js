66095896eabe367c409b7b23e40b5d9e7af3caaf
Shomirsaidov <abubakr.shomirsadov2@gmail.com>
2026-03-21 16:52:25 +0500

    Switch broadcast links to bot-mediated /start command for 100% reliability

--- utils/telegramBot.js
+++ utils/telegramBot.js
@@ -84,14 +84,14 @@ async function sendBroadcast(text, rideId = null) {
         inlineKeyboard.push([
             { 
                 text: '🚀 Подробнее в приложении', 
-                url: `https://t.me/${botUsername}?startapp=ride_${rideId}` 
+                url: `https://t.me/${botUsername}?start=ride_${rideId}` 
             }
         ]);
     } else {
         inlineKeyboard.push([
             { 
                 text: '📱 Открыть приложение', 
-                url: `https://t.me/${botUsername}?startapp` 
+                url: `https://t.me/${botUsername}?start=app` 
             }
         ]);
     }
